e6692d7b0c65964aa805710d02d04923e9e3e3c3
VSergeenko <titanium.svn@yandex.ru>
2015-11-16 13:07:03 +0400

    LOGNAME: change config and code

--- logname-/logname-svn.c
+++ logname-/logname-svn.c
@@ -3,26 +3,22 @@
 
 // podklu4aem biblioteki
 #include <stdio.h>
-#include <sys/types.h>
-#include <pwd.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 // glabnaya funkciya
 int main(int argc, char const *argv[])
 {
-	//polu4aem strukturu /etc/passwd
-	//getuid() - 4islovoi ID polzovatelya
-	struct passwd *p = getpwuid(getuid());
+	//polu4aem imya polvozatelya
+	char *username=getenv("USER");
 	//proveryaem na NULL
-	if (p == NULL)
+	if (username == NULL)
 		{
 		//vivod oshibka, vozvrat 1
 		printf("Sorry, ERROR\n");
 		return 1;
 		}
 	//ina4e vivod imeni polzovatelya
-	printf("Username: %s", p->pw_name);
+	printf("Username: %s", username);
 	printf("\n");
 	//vozvrat 0, 4to vse uspeshno
 	return 0;
