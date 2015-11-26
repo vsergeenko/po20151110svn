233da11c1a0c5be5f13dd5dd8cb12a113f8b6022
VSergeenko <titanium.svn@yandex.ru>
2015-11-26 13:40:07 +0400

    PWDX: code

--- pwdx-/pwdx-svn.c
+++ pwdx-/pwdx-svn.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+
+//function
+int mypwdx()
+{
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
-	int pathproc;
-	//4itaem 1 argument v stroke
-	char *cmdarg = argv[1];
 	//sozdaem bufer
-	char buf[256];
-	//sozdaem stroku processa
-	char pathlen[256] = srtcat(srtcat("/proc/",argv[1]),"/cwd");
-	//delaem sys vizov
-	pathproc = readlink(pathlen, buf, sizeof(buf));
-	printf("%s\n", buf);
+	char buff[256];
+	printf("%s\n", buff);
 	return 0;
 }
