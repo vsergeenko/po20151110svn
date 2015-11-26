7b9ca878ee967a2fd5d7b0eb9d325d9017bc8231
VSergeenko <titanium.svn@yandex.ru>
2015-11-26 14:11:58 +0400

    PWDX: code proverki

--- pwdx-/pwdx-svn.c
+++ pwdx-/pwdx-svn.c
@@ -11,12 +11,27 @@ int mypwdx()
 
 int main(int argc, char *argv[])
 {
+	//proverka nali4iya argumente
+	if (argc < 2)
+	{
+	  printf("Vvedite nomer processa!\n");
+	  return 1;
+	}
 	//sozdaem bufer dlya vivoda
-//	char buff[256];
+	char buff[256];
 	//formiruem stroku processa
 	char part1[256]="/proc/";
 	strcat(part1, argv[1]);
 	strcat(part1, "/cwd");
-	printf("%s\n", part1);
+	//delaem sys vizov (man 2 readlink)
+	int len;
+	len = readlink(part1, buff, sizeof(buff));
+	//proverka dlini
+	if (len < 0)
+	{
+	  printf("No exist process: %s\n", argv[1]);
+	  return 1;
+	}
+	printf("%s\n", buff);
 	return 0;
 }
