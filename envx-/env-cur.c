810e6f44f582cc0061091ae42ad3aa037515d4d3
VSergeenko <titanium.svn@yandex.ru>
2015-11-26 16:46:53 +0400

    ENVX: create code

---
+++ envx-/env-cur.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern char **environ;
+
+int main(int argc, char **argv)
+{
+  int i;
+
+  if( environ != NULL )
+    for( i=0; environ[i]; i++ )
+      printf("%s\n",environ[i]);
+
+  return 0;
+}
+
