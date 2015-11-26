251d253b63f26bec32568a3b74d39adcb7884108
VSergeenko <titanium.svn@yandex.ru>
2015-11-26 13:59:11 +0400

    PDWX: change code

--- pwdx-/pwdx-svn.c
+++ pwdx-/pwdx-svn.c
@@ -9,10 +9,14 @@ int mypwdx()
 	return 0;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	//sozdaem bufer
-	char buff[256];
-	printf("%s\n", buff);
+	//sozdaem bufer dlya vivoda
+//	char buff[256];
+	//formiruem stroku processa
+	char part1[256]="/proc/";
+	strcat(part1, argv[1]);
+	strcat(part1, "/cwd");
+	printf("%s\n", part1);
 	return 0;
 }
