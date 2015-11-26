d292fb91f2bff4af0c3ea1611db64ef78743ac8e
VSergeenko <titanium.svn@yandex.ru>
2015-11-26 18:13:46 +0400

    ENVX: change code

--- envx-/envx-svn.c
+++ envx-/envx-svn.c
@@ -11,21 +11,34 @@ int main(int argc, char *argv[])
 	  printf("Vvedite nomer processa!\n");
 	  return 1;
 	}
-        //sozdaem bufer dlya vivoda
-//        char buff[256];
+	//4itaem argument
 	char *cmdarg2 = argv[1];
-        //formiruem put k failu
+        //formiruem put` k failu
         char part2[256]="/proc/";
         strcat(part2, cmdarg2);
         strcat(part2, "/environ");
 	//open file env only read
 	FILE *myfile;
 	myfile = fopen(part2, "r");
-	if ( !myfile)
+	if ( !myfile )
 	{
 	  printf("The file isn't present or it is unavailable\n");
 	  return 1;
 	}
-//	printf("%s\n", myfile);
+	char c;
+	do
+	{
+	  c = fgetc(myfile);
+	  //esli nado, to menyaem konec stroki na perenos stroki
+	  if ( c == '\0' )
+	  {
+	    c = '\n';
+	  }
+	  printf("%c", c);
+	}
+	while ( c != EOF );
+	//zakrivaem file
+	fclose(myfile);
+//	printf("%s\n", bufile);
 	return 0;
 }
